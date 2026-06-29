f6fac57daac40003feb7f869b6c1ad49f20e3450
PavlNekrasov <95914807+PavlNekrasov@users.noreply.github.com>
2026-06-29 14:08:42 +0300

    Delete tpm_fuzzing directory

--- tpm_fuzzing/gdb.sh
+++
@@ -1,1 +0,0 @@
-gdb -ex 'set environment LD_PRELOAD /home/me/tpm_fuzzing/feeder/derand.so /home/me/tpm_fuzzing/feeder/feeder.so' -ex "set args < $1" ./src/tpm_server
--- tpm_fuzzing/run.sh
+++
@@ -1,1 +0,0 @@
-LD_PRELOAD="/home/me/tpm_fuzzing/feeder/derand.so /home/me/tpm_fuzzing/feeder/feeder.so" ./src/tpm_server < $1
